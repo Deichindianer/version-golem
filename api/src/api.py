a8bca00a05af8e3deac0d243c14d516f3a63a493
Philipp Boeschen <philipp.boeschen@tui.com>
2020-08-01 16:53:49 +0200

    Splitting the api server and the version updater

---
+++ api/src/api.py
@@ -0,0 +1,57 @@
+import json
+
+from flask import Flask, jsonify, request
+from flask_restful import Resource, Api
+from tinydb import TinyDB, Query
+
+
+app = Flask(__name__)
+api = Api(app)
+db = TinyDB("db.json")
+
+
+def update_repository(repository):
+    """
+    Takes in a dict with a new repository and updates the thing :)
+    """
+
+
+class Repository(Resource):
+
+    def get(self, name):
+        repository = Query()
+        return db.search(repository.name == name)
+
+    def put(self, name):
+        request_data = request.get_json(force=True)
+        repository = Query()
+        db.upsert(request_data, repository.name == request_data["name"])
+        return jsonify(request_data)
+
+
+class Repositories(Resource):
+
+    def get(self):
+        repository = Query()
+        return db.all()
+
+    def post(self):
+        request_data = request.get_json(force=True)
+        author = request_data["author"]
+        name = request_data["name"]
+        repository = Query()
+        db.upsert(
+            {
+                "name": name,
+                "author": author,
+                "tracked_version": request_data["tracked_version"],
+                "latest_version": "To be updated"
+            },
+            repository.name == name
+        )
+        return jsonify(request_data)
+
+
+api.add_resource(Repositories,"/repositories")
+api.add_resource(Repository,"/repository/<string:name>")
+app.run(host="0.0.0.0", debug=True)
--- version-updater/src/versions.py
+++ version-updater/src/versions.py
@@ -1,12 +1,17 @@
+import os
+import time
+
 import requests
 
 
 def get_latest_version(repo_slug):
+    print("Getting latest version...")
     latest_release = requests.get(f"https://api.github.com/repos/{repo_slug}/releases/latest").json()
     if latest_release["tag_name"].startswith("v"):
         version = latest_release["tag_name"][1:]
     else:
         version = latest_release["tag_name"]
+    print(f"Got {version}")
     return {
         "release_url": latest_release["html_url"],
         "version": version,
@@ -15,16 +20,24 @@ def get_latest_version(repo_slug):
 
 
 def update_versions():
-    repositories = requests.get("localhost:5000/repositories")
+    api_endpoint = os.environ["API_ENDPOINT"]
+    print(f"Running against {api_endpoint}")
+    repositories = requests.get(f"{api_endpoint}/repositories").json()
     for repo in repositories:
+        print(f"Looking through {repo['name']}")
         name = repo["name"]
         repo_slug = f"{repo['author']}/{name}"
         latest_version = get_latest_version(repo_slug)
         if repo["latest_version"] != latest_version["version"]:
+            print(f"{repo['name']} is outdated!")
+            print(repo)
             repo["latest_version"] = latest_version
-            requests.put(f"localhost:5000/repositories/{name}", json=repo)
+            result = requests.put(f"{api_endpoint}/repository/{name}", json=repo)
+            print(result)
 
 
 if __name__ == "__main__":
     print("Running update versions now")
-    update_versions()
+    while True:
+        update_versions()
+        time.sleep(15)
