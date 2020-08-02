1c2ad830509a159b978db31b6080264bf04bbe55
Philipp Boeschen <philipp.boeschen@tui.com>
2020-08-03 00:00:17 +0200

    Adapting the version updater to use the new API schema

--- version-updater/src/versions.py
+++ version-updater/src/versions.py
@@ -1,17 +1,28 @@
+import logging
 import os
+import sys
 import time
 
 import requests
 
 
+logger = logging.getLogger(__name__)
+ch = logging.StreamHandler()
+logger.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+ch.setFormatter(formatter)
+ch.setLevel(logging.INFO)
+logger.addHandler(ch)
+
+
 def get_latest_version(repo_slug):
-    print("Getting latest version...")
+    logger.info(f"Getting latest version for {repo_slug}")
     latest_release = requests.get(f"https://api.github.com/repos/{repo_slug}/releases/latest").json()
     if latest_release["tag_name"].startswith("v"):
         version = latest_release["tag_name"][1:]
     else:
         version = latest_release["tag_name"]
-    print(f"Got {version}")
+    logger.info(f"Got {version} for {repo_slug}")
     return {
         "release_url": latest_release["html_url"],
         "version": version,
@@ -19,25 +30,33 @@ def get_latest_version(repo_slug):
     }
 
 
-def update_versions():
-    api_endpoint = os.environ["API_ENDPOINT"]
-    print(f"Running against {api_endpoint}")
-    repositories = requests.get(f"{api_endpoint}/repositories").json()
-    for repo in repositories:
-        print(f"Looking through {repo['name']}")
-        name = repo["name"]
-        repo_slug = f"{repo['author']}/{name}"
-        latest_version = get_latest_version(repo_slug)
-        if repo["latest_version"] != latest_version["version"]:
-            print(f"{repo['name']} is outdated!")
-            print(repo)
-            repo["latest_version"] = latest_version
-            result = requests.put(f"{api_endpoint}/repository/{name}", json=repo)
-            print(result)
+def update_version(repo):
+    logger.debug(f"Checking version of {repo['name']}")
+    latest_version = get_latest_version(f"{repo['author']}/{repo['name']}")
+    logger.debug(
+        f"Checking {repo['latest_version']}(current) against {latest_version['version']}(latest)"
+    )
+    if repo["latest_version"] != latest_version["version"]:
+        logger.info(f"{repo['name']} is outdated!")
+        repo["latest_version"] = latest_version["version"]
+        repo["latest_release_url"] = latest_version["release_url"]
+        repo["latest_version_publish_date"] = latest_version["published_at"]
+        logger.debug(f"Updating version of {repo['name']} with {latest_version['version']}")
+        result = requests.put(f"{api_endpoint}/repository/{repo['name']}", json=repo)
 
 
 if __name__ == "__main__":
-    print("Running update versions now")
+    for env_var in ["API_ENDPOINT", "UPDATE_INTERVAL"]:
+        if not os.environ.get(env_var): 
+            logger.critical(
+                f"Make sure {env_var} environment variable is set!"
+            )
+            sys.exit(1)
     while True:
-        update_versions()
-        time.sleep(15)
+        api_endpoint = os.environ["API_ENDPOINT"]
+        logger.debug(f"Running against {api_endpoint}")
+        repositories = requests.get(f"{api_endpoint}/repositories").json()
+        for repo in repositories:
+            update_version(repo)
+        logger.info(f"Waiting for {os.environ['UPDATE_INTERVAL']} seconds.")
+        time.sleep(int(os.environ["UPDATE_INTERVAL"]))
